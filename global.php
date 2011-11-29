c720bb38259d94ff7e5654a6eabcd81cce95f2da
ekilnu <private.server.shakes.and.fidget@googlemail.com>
2011-11-29 18:04:42 +0100

    templates/index.tpl created. index.php editet.

--- global.php
+++ global.php
@@ -8,5 +8,9 @@
  * @license   Attribution-NonCommercial-NoDerivs 3.0 Unported <http://creativecommons.org/licenses/by-nc-nd/3.0/>
  * @version   1.0.0
  */
-require_once(dirname(__FILE__).'/global.php');
+
+/** we need php version 5.3.x and higher! **/
+if (!strnatcmp(phpversion(), '5.3.0')) {
+  die('You need PHP VERSION 5.3.x or 5.4.x to running');
+}
 ?>
